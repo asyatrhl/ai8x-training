30d3ea53ddf59c587f40d555220a76d6e89bdc56
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-03-02 13:31:25 +0300

    Update train_cifar100_1.sh

--- regression-test/regression-scripts/train_cifar100_1.sh
+++ regression-test/regression-scripts/train_cifar100_1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --epochs 60 --optimizer Adam --lr 0.00032 --wd 0 --compress regression-test-policies/schedulscriptse-cifar100-1.yaml --model ai85simplenet --dataset CIFAR100 --device MAX78000 --batch-size 32 --print-freq 100 --validation-split 0 --qat-policy None --use-bias "$@"
+python train.py --epochs 60 --optimizer Adam --lr 0.00032 --wd 0 --compress regression-policies/schedule-cifar100-v1.yaml --model ai85simplenet --dataset CIFAR100 --device MAX78000 --batch-size 32 --print-freq 100 --validation-split 0 --qat-policy None --use-bias --enable-tensorboard "$@"

f3fa2e8146c46995845f6887f6e5e7049dc023cf
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-03-06 16:38:11 +0300

    Add files via upload

---
+++ create_test_script.py
@@ -0,0 +1,36 @@
+import os
+
+def joining(list):
+    # Join based on the ' ' delimiter
+    str = ' '.join(list)
+    return str
+
+# Folder containing the files to be concatenated
+folder_path = r"C:\Users\aturhal\Desktop\ai\source\ai8x-training\scripts"
+
+# Output file name and path
+output_file_path = r"C:\Users\aturhal\Desktop\ai\source\ai8x-training\scripts\output_file.sh"
+
+# Regular expression pattern to match the "asya" script with any number
+
+# Loop through all files in the folder
+with open(output_file_path, "w") as output_file:
+    for filename in os.listdir(folder_path):
+        # Check if the file is a text file
+        if filename.startswith("train"):
+            # Open the file and read its contents
+            with open(os.path.join(folder_path, filename)) as input_file:
+                contents = input_file.read()
+
+            temp = contents.split()
+            i = temp.index('--epochs')
+            
+            
+            #temp[i+1] = str(int(temp[i+1])*10/100) 
+            temp[i+1] = str(5)
+            temp.append("\n")
+            contents = joining(temp)
+            # Replace the number in the "--epochs" script
+
+            # Write the contents to the output file
+            output_file.write(contents)

40546aa4e5a552d5ab361760ea19c47741ec7208
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-03-13 15:14:51 +0300

    Add files via upload

---
+++ create_onnx_scripts.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+
+def joining(list):
+    # Join based on the ' ' delimiter
+    str = ' '.join(list)
+    return str
+
+folder_path= r"C:\Users\aturhal\Desktop\ai\source\ai8x-training\logs"
+output_file_path = r"C:\Users\aturhal\Desktop\ai\source\ai8x-training\scripts\onnx_scripts.sh"
+train_path = r"C:\Users\aturhal\Desktop\ai\source\ai8x-training\scripts\output_file.sh"
+
+logs_list = os.listdir(folder_path)
+
+models = []
+datasets = []
+model_path = []
+
+with open(output_file_path, "w") as onnx_scripts:
+    with open(train_path) as input_file:
+        contents = input_file.read()
+
+    contents = contents.split()
+    contents = np.array(contents)
+    j = [i+1 for i in range(len(contents)) if contents[i]=='--model']
+    for index in j:
+        models.append(contents[index])
+
+    j = [i+1 for i in range(len(contents)) if contents[i]=='--dataset']
+    for index in j:
+        datasets.append(contents[index])
+
+    for file in logs_list:
+        temp = './logs/{}/checkpoint.pth.tar'.format(file)
+        model_path.append(temp)
+
+
+    final = []
+    for i in range(len(models)):
+        temp = "python train.py --model --dataset --evaluate --exp-load-weights-from --device MAX78000 --summary onnx "
+        temp = temp.split()
+        temp.insert(3, models[i] )
+        temp.insert(5, datasets[i])
+        temp.insert(8, model_path[i])
+        temp.append("\n")
+
+        onnx_scripts.write(joining(temp))

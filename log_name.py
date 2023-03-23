5bd244eb6eb0a4f48774f69e2e8946f482b49ab1
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-03-23 11:40:25 +0300

    Update log_name.py

--- log_name.py
+++ log_name.py
@@ -1,18 +1,11 @@
 import os
 from create_test_script import log_file_names
 
-sh_path = r'/home/asyaturhal/actions-runner/_work/ai8x-training/ai8x-training/logs'
+log_path = r'/home/asyaturhal/actions-runner/_work/ai8x-training/ai8x-training/logs'
 
-with open(sh_path, "r") as f:
-    lines = f.readlines()
-
-log_list = os.listdir(sh_path)
+log_list = os.listdir(log_path)
 
 for (log, name) in zip(log_list, log_file_names) :
-    path1 = sh_path +  '/' + log
-    new_path = sh_path +  '/' + name
-    print(path1)
-    print(new_path)
-
+    path1 = log_path +  '/' + log
+    new_path = log_path +  '/' + name
     os.rename(path1, new_path)
-

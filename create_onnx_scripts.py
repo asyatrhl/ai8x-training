e1cd6ca48f8b325d4eb848753643ee338a740f42
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-04-07 15:56:02 +0300

    Update create_onnx_scripts.py

--- create_onnx_scripts.py
+++ create_onnx_scripts.py
@@ -10,20 +10,21 @@ folder_path= r"/home/asyaturhal/actions-runner/_work/ai8x-training/ai8x-training
 output_file_path = r"/home/asyaturhal/actions-runner/_work/ai8x-training/ai8x-training/scripts/onnx_scripts.sh"
 train_path = r"/home/asyaturhal/actions-runner/_work/ai8x-training/ai8x-training/scripts/output_file.sh"
 
-logs_list = os.listdir(folder_path)
-print(logs_list[0])
-print(logs_list[1])
+logs_list = sorted(os.listdir(folder_path))
 
 models = []
 datasets = []
 model_path = []
+bias = []
 
 with open(output_file_path, "w") as onnx_scripts:
     with open(train_path) as input_file:
         contents = input_file.read()
-
+    lines = contents.split("#!/bin/sh ")
+    lines = lines[1:]
     contents = contents.split()
     contents = np.array(contents)
+
     j = [i+1 for i in range(len(contents)) if contents[i]=='--model']
     for index in j:
         models.append(contents[index])
@@ -32,18 +33,25 @@ with open(output_file_path, "w") as onnx_scripts:
     for index in j:
         datasets.append(contents[index])
 
+    for i in range(len(lines)):
+        if "--use-bias" in lines[i]:
+            bias.append("--use-bias")
+        else:
+            bias.append("")
+
     for file in logs_list:
         temp = './logs/{}/checkpoint.pth.tar'.format(file)
         model_path.append(temp)
 
-
-    final = []
     for i in range(len(models)):
-        temp = "python train.py --model --dataset --evaluate --exp-load-weights-from --device MAX78000 --summary onnx --use-bias"
+        temp = "python train.py --model --dataset --evaluate --exp-load-weights-from --device MAX78000 --summary onnx "
+
         temp = temp.split()
         temp.insert(3, models[i] )
         temp.insert(5, datasets[i])
         temp.insert(8, model_path[i])
+        temp.append("--summary-filename {}{}onnx".format(models[i],datasets[i]))
+        temp.append(bias[i])
         temp.append("\n")
 
         onnx_scripts.write(joining(temp))

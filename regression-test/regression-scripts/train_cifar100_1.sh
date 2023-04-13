1f7897c74ab8da7bc1c92f8f88a467230d835ea6
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-04-13 14:16:20 +0300

    Delete regression-test directory

--- regression-test/regression-scripts/train_cifar100_1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python train.py --epochs 60 --optimizer Adam --lr 0.00032 --wd 0 --compress regression-policies/schedule-cifar100-v1.yaml --model ai85simplenet --dataset CIFAR100 --device MAX78000 --batch-size 32 --print-freq 100 --validation-split 0 --qat-policy None --use-bias --enable-tensorboard "$@"
